397ceba6f85913d1e9b770fb86f6194ec17decbf
pari <otpari.cs@gmail.com>
2015-11-19 07:51:19 +0000

    Retuen type changed for strlen function

--- strlen.c
+++ strlen.c
@@ -14,6 +14,6 @@ int main(void)
       "Tutorials Point"
    };
 for (i = 0; i < 2; ++i)
-printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+printf("string lenght of %s = %lu\n", s[i], my_strlen(s[i]));
 return 0; }
 ￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼
